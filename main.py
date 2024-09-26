dc3f9926b4bc2e6787a21bd0c294f15da5fb2f15
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2024-09-26 11:00:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-n = input() 
-first_num = int(n[0])
-second_num =  int(n[1])
-third_num =  int(n[2])
-fourth_num =  int(n[3])
-fifth_num =  int(n[4])
-sixth_num =  int(n[5])
-seventh_num =  int(n[6])
-eighth_num = int(n[7])
+n = int(input()) 
+first_num = n // 10000000
+second_num =  n // 1000000
+third_num =   n // 100000
+fourth_num =   n // 10000
+fifth_num =   n // 1000
+sixth_num =   n // 100
+seventh_num = n // 10
+eighth_num = n % 10
 sum = first_num + second_num + third_num + fourth_num + fifth_num + sixth_num + seventh_num + eighth_num
 print(f"Сумма всех цифр числа {n} равна {sum}")
